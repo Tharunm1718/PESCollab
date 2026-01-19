bd003455655a63c090333fc59286015fb2cdfb47
Tharunm1718 <tharunm1817@gmail.com>
2026-01-19 23:45:09 +0530

    Fixed Fileupdate and MyProfile View

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 let port = 3000;
 app.use(cors({
-  origin: ['http://localhost:5173',"https://pescollab.vercel.app"],
+  origin:"https://pes-collab.vercel.app",
   credentials: true
 }));
 
